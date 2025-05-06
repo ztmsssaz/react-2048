8d1c74c45f1d37f352e6b155f5e4735ff65272e2
unknown <zarei.mrabolfazl@gmail.com>
2025-05-06 12:30:43 +0330

    add tailwind to project

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type {Config} from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', // مسیر فایل‌های پروژه‌ات رو اینجا وارد کن
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
